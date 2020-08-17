f17d7c052e0247a26716602006ab8aefa89f22b7
anyProtein <15629052192@163.com>
2020-08-17 20:54:22 +0800

    新增测试

---
+++ src/Test/Tset.java
@@ -0,0 +1,13 @@
+package Test;
+
+/**
+ * JavaSE_Learning.Test
+ * Created on 2020/8/17 20:51
+ *
+ * @author Protein
+ */
+public class Tset {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
